a1a7fa4d1956ccb40bbd2d387b1d1e81273f19bc
Thomas Darde <thomas@rougecardinal.fr>
2019-02-01 05:37:18 +0100

    Allow to setup mojito host

--- lib/mojito_import.rb
+++ lib/mojito_import.rb
@@ -4,12 +4,17 @@ module MojitoImport
   class Importer
     attr_accessor :import_scenario_identifier, :access_token
     attr_accessor :additional_row_data, :button_name
+    attr_accessor :mojito_host
 
-    def self.create(import_scenario_identifier:  , access_token: , additional_row_data: {}, button_name: "Upload your file")
+    def self.create(import_scenario_identifier:  ,
+                    access_token: , additional_row_data: {},
+                    button_name: "Upload your file",
+                    mojito_host: "https://mojito-import.herokuapp.com")
       importer = MojitoImport::Importer.new
       importer.access_token = access_token
       importer.additional_row_data = additional_row_data
       importer.button_name = button_name
+      importer.mojito_host = mojito_host
       importer.import_scenario_identifier = import_scenario_identifier
       importer
     end
@@ -21,6 +26,11 @@ module MojitoImport
 
 
     private
+
+    def method_name
+
+    end
+
     def iframe_src
       # On query le serveur avec cet url
       # IL renvoi un id crypté de l'objet "import"
@@ -28,7 +38,7 @@ module MojitoImport
       # pour le moment l'id n'est pas crypté ..
       # Il faut maintenant créer l'iframe correspondant en bougeant du code
 
-      "https://mojito-import.herokuapp.com/imports/#{import_id}/iframe?access_token=#{access_token}"
+      "#{mojito_host}/imports/#{import_id}/iframe?access_token=#{access_token}"
     end
 
     def import_id
@@ -36,7 +46,7 @@ module MojitoImport
       data = {additional_row_data: additional_row_data, import_scenario_identifier: self.import_scenario_identifier}
       # RestClient::Resource.new( , verify_ssl: false, log: Logger.new(STDOUT)).post "https://mojito-import.test/api/v1/new_import", data.to_json, {content_type: :json, accept: :json, :Authorization => "Bearer #{access_token}"}
 
-      remote_endpoint_url = "https://mojito-import.herokuapp.com/api/v1/new_import"
+      remote_endpoint_url = "#{mojito_host}/api/v1/new_import"
       RestClient::Resource.new( remote_endpoint_url, verify_ssl: false, log: Logger.new(STDOUT)).post data.to_json, {content_type: :json, accept: :json, :Authorization => "Bearer #{access_token}"}
     end
   end
