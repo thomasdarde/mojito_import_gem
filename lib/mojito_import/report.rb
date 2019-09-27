a6b3ebe9f40161b29c4e7c2e12c98a99cfd6bf9d
Thomas Darde <thomas@rougecardinal.fr>
2019-09-27 10:59:40 +0200

    Cleanup code

--- lib/mojito_import/report.rb
+++ lib/mojito_import/report.rb
@@ -1,6 +1,5 @@
 module MojitoImport
   class Report
-
     attr_accessor :data_errors, :general_errors, :import_id
 
     def initialize(import_id)
@@ -14,30 +13,24 @@ module MojitoImport
     end
 
     def add_data_error(mojito_id, field, error)
-    # reload!; report = MojitoImport::Report.new('1234'); report.add_data_error('123', 'name', 'your name sucks')
-    hash_element = data_errors.detect { |error_hash| error_hash["mojitoId"] == mojito_id }
+      # reload!; report = MojitoImport::Report.new('1234'); report.add_data_error('123', 'name', 'your name sucks')
+      hash_element =
+        data_errors.detect { |error_hash| error_hash["mojitoId"] == mojito_id }
 
-    if hash_element.nil?
-      new_error_hash = {
-        "mojitoId" => mojito_id,
-        field => [error]
-      }
-      data_errors << new_error_hash
-    else
-      hash_element[field] ||= []
-      hash_element[field] << error
+      if hash_element.nil?
+        new_error_hash = { "mojitoId" => mojito_id, field => [error] }
+        data_errors << new_error_hash
+      else
+        hash_element[field] ||= []
+        hash_element[field] << error
+      end
+    end
+
+    def find_errors_for_object(mojito_id)
+      data_error = data_errors.detect { |data| data["mojitoId"] == mojito_id }
+      return if data_error.nil?
+
+      data_error.except("mojitoId")
     end
   end
-
-  def find_errors_for_object(mojito_id)
-    data_error = data_errors.detect{|data| data["mojitoId"] == mojito_id}
-    return if data_error.nil?
-
-    data_error.except("mojitoId")
-  end
-
-
-
-
-end
 end
