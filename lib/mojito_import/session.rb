2dbd4af3f4972c537ffe257a6e38aba25026d328
Tristan Belin <tristan@rougecardinal.fr>
2020-01-10 16:09:07 +0100

    return token if success; return parsed response otherwise

--- lib/mojito_import/session.rb
+++ lib/mojito_import/session.rb
@@ -9,9 +9,14 @@ module MojitoImport
 
       request = RestClient::Resource.new('http://www.mojito-import.test', accept: :json,content_type: :json, Authorization: "Bearer #{access_token}", verify_ssl: false)
 
-      token = request["api/v1/import_session_token?access_token=#{access_token}"].post(data)
-
-      return JSON.parse(token)["token"]
+      response = request["api/v1/import_session_token?access_token=#{access_token}"].post(data) { |response, request, result|
+        case response.code
+        when 200, 201
+          return JSON.parse(response)["token"]
+        else
+          return JSON.parse(response)
+        end
+      }
     end
   end
 end
