71158bd2d7a5f94fb21e15a55865eff30cd2600a
Thomas Darde <thomas@rougecardinal.fr>
2020-01-14 15:34:26 +0100

    Handle more cases, remove some code

--- lib/mojito_import/session.rb
+++ lib/mojito_import/session.rb
@@ -1,22 +1,27 @@
 require "rest-client"
 module MojitoImport
   class Session
-    attr_accessor :access_token, :session_bucket, :import_scenario_identifier
 
-    def self.get_token(import_scenario_identifier, access_token, session_bucket)
+
+    def self.get_token(import_scenario_identifier, import_session_token, session_bucket, use_dev_host: false)
       data = { import_scenario_identifier: import_scenario_identifier, session_bucket: session_bucket }
-      session_token_endpoint = "http://www.mojito-import.test/api/v1/import_session_token"
 
-      request = RestClient::Resource.new('http://www.mojito-import.test', accept: :json,content_type: :json, Authorization: "Bearer #{access_token}", verify_ssl: false)
+      mojito_host = use_dev_host ? 'http://www.mojito-import.test' : 'https://www.mojito-import.com'
 
-      response = request["api/v1/import_session_token?access_token=#{access_token}"].post(data) { |response, request, result|
+      request = RestClient::Resource.new(mojito_host, accept: :json,content_type: :json, Authorization: "Bearer #{import_session_token}", verify_ssl: false)
+
+      response = request["api/v1/import_session_token?access_token=#{import_session_token}"].post(data) { |response, request, result|
         case response.code
         when 200, 201
           return JSON.parse(response)["token"]
+        when 500
+          return {"error when retrieving token" => response.body.inspect}
         else
           return JSON.parse(response)
         end
       }
     end
+
+
   end
 end
